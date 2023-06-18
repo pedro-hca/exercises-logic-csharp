1f2e49b33660b83f0331733769a94903ffdb6e0c
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-18 18:10:38 -0300

    Exercise_02 solution

---
+++ Exercise_02/Exercise_02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AreaCalculator();
+        }
+        static void AreaCalculator()
+        {
+            Console.Clear();
+
+            double pi = 3.14159;
+            Console.Write("Digite o valor do raio");
+            double raio = double.Parse(Console.ReadLine());
+
+            double area = pi * (Math.Pow(raio, 2));
+
+            Console.Write("A= " + area.ToString("F2"));
+
+        }
+    }
+}
