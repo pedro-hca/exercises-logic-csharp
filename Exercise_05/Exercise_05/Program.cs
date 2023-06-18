9ce577ca760474074c3dfb5eab097fda64cbe371
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-18 18:25:43 -0300

    Exercise_05 solution

---
+++ Exercise_05/Exercise_05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Senha();
+        }
+
+        static void Senha()
+        {
+
+            Console.Clear();
+
+            int senhavalida = 2002;
+
+            Console.WriteLine("Digite a senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != senhavalida)
+            {
+                Console.Clear();
+                Console.WriteLine("Senha inválida");
+
+                Console.WriteLine("Tente novamente:");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.Clear();
+            Console.Write("Acesso permitido");
+
+        }
+    }
+}
