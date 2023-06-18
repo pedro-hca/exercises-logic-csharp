d78bf476f36d02fd4d55c25a39d9317497f995f7
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-18 18:27:32 -0300

    Exercise_06 solution

---
+++ Exercise_06/Exercise_06/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercise_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Intervalo();
+        }
+
+        static void Intervalo()
+        {
+
+            Console.Clear();
+            Console.WriteLine("Digite a quantidade de numeros: ");
+            int n = int.Parse(Console.ReadLine());
+            int dentro = 0;
+            int fora = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite um numero: ");
+                int x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20)
+                {
+                    dentro += 1;
+                }
+                else
+                {
+                    fora += 1;
+                }
+            }
+
+            Console.WriteLine($"{dentro} in");
+            Console.WriteLine($"{fora} out");
+
+        }
+    }
+}
