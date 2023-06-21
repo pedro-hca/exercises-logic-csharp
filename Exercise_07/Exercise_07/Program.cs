0fe5b96f668d2db16ea1eda8ded226790298d977
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-20 22:24:48 -0300

    Exercise_07 solution

---
+++ Exercise_07/Exercise_07/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            double A = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            double B = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            double C = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            double triangulo = A * C / 2.0;
+            double circulo = Math.Pow(C, 2) * 3.13159;
+            double trapezio = (((A + B) * C) / 2);
+            double quadrado = Math.Pow(B, 2);
+            double retangulo = ((A + B) / 2) * C;
+
+            Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
