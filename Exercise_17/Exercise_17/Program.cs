e682d2850bfb7f15609d6ea98c8a9571b0f8a643
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-24 18:25:24 -0300

    Exercise_17 solution

---
+++ Exercise_17/Exercise_17/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= valor; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
