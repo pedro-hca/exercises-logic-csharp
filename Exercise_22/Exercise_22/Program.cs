bb66e35a40c3771e471818c893986fbef6fa1ab5
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-24 20:14:13 -0300

    Exercise_22 solution

---
+++ Exercise_22/Exercise_22/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                int a = i;
+                int b = i * i;
+                int c = i * i * i;
+                Console.WriteLine($"{a} {b} {c}");
+            }
+        }
+    }
+}
