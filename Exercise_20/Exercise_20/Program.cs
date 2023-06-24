775c8d5eaf42670149354a1162191ec5b9fff907
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-24 19:38:41 -0300

    Exercise_20 solution

---
+++ Exercise_20/Exercise_20/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int fatorial = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                fatorial = fatorial * i;
+            }
+            Console.WriteLine(fatorial);
+        }
+    }
+}
