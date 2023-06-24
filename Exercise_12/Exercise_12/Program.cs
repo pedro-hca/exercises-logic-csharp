fdde59b2345d9a40840a02e16d5e8f52984f3767
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-24 16:02:45 -0300

    Exercise_12 solution

---
+++ Exercise_12/Exercise_12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (num > 0.0 && num <= 25)
+            {
+                Console.WriteLine("Intervalo [0,25]");
+            }
+            else if (num > 25.0 && num <= 50)
+            {
+                Console.WriteLine("Intervalo [25,50]");
+            }
+            else if (num >= 50.0 && num <= 75)
+            {
+                Console.WriteLine("Intervalo [50,75]");
+            }
+            else if (num >= 75.0 && num <= 100)
+            {
+                Console.WriteLine("Intervalo [75,100]");
+            }
+            else
+            {
+                Console.WriteLine("Fora de Intervalo");
+            }
+        }
+    }
+}
+
