b7d66aa4444e0267d028324a482422bfc4c55533
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-24 15:49:21 -0300

    Exercise_10 solution

---
+++ Exercise_11/Exercise_11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num = Console.ReadLine().Split(' ');
+            int a = int.Parse(num[0]);
+            int b = int.Parse(num[1]);
+            int resultado = 0;
+
+            if (a < b)
+            {
+                resultado = b - a;
+                Console.WriteLine($"O JOGO DUROU {resultado} HORA(S)");
+            }
+            else
+            {
+                resultado = (24 - a) + b;
+                Console.WriteLine($"O JOGO DUROU {resultado} HORA(S)");
+            }
+        }
+    }
+}
+
