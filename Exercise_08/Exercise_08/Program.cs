0f7a7fe2828f80b74971202dca3529bd91373a90
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-24 13:41:30 -0300

    Exercise_08 solution

---
+++ Exercise_08/Exercise_08/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+
+            if (num >= 0)
+            {
+                Console.WriteLine("NAO NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+        }
+    }
+}
