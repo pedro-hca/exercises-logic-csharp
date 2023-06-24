77c1eb6df53177a2ae26dfb67096faeac436732f
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-24 15:29:22 -0300

    Exercise_09 solution

---
+++ Exercise_09/Exercise_09/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0 || num == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
