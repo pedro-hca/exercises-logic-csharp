18fab5cbdaeec5e79ccc3956a0145c39b81ed59a
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-24 20:06:56 -0300

    Exercise_21 solution

---
+++ Exercise_21/Exercise_21/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
