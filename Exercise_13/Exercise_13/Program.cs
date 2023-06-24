d016dd93586e5b054b88d7b5356783129d4d2119
pedro-hca <pedroandradecontato@hotmail.com>
2023-06-24 16:11:09 -0300

    Exercise_13 solution

---
+++ Exercise_13/Exercise_13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercise_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num = Console.ReadLine().Split(' ');
+            double x = double.Parse(num[0]);
+            double y = double.Parse(num[1]);
+
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else
+            {
+                Console.WriteLine("Origem");
+            }
+        }
+    }
+}
